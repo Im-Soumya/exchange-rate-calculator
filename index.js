23944a2ccd5ce9cd07123446b139f32910105d96
Deathstalker <iamsoumyass@gmail.com>
2022-02-03 11:53:52 +0530

    finished

--- index.js
+++ index.js
@@ -7,20 +7,19 @@ const amount2 = document.querySelector('#amount2')
 const rateText = document.querySelector('.rate')
 const swap = document.querySelector('.swap-btn')
 
-function calculate() {
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      const rate = data.rates[currency2.value] / data.rates[currency1.value]
-      rateText.innerText = `1 ${currency1.value} = ${currency2.value}`
-      amount2.value = (amount1.value * rate).toFixed(2)
-    })
+const calculate = async() => {
+  const response = await fetch(url)
+  const data = await response.json()
+
+  let rate = (data.rates[currency2.value] / data.rates[currency1.value]).toFixed(2)
+  rateText.innerText = `1 ${currency1.value} = ${rate} ${currency2.value}`
+  amount2.value = (amount1.value * rate).toFixed(2)
 }
 
-amount1.addEventListener('input', calculate)
-amount2.addEventListener('input', calculate)
 currency1.addEventListener('change', calculate)
 currency2.addEventListener('change', calculate)
+amount1.addEventListener('input', calculate)
+amount2.addEventListener('input', calculate)
 swap.addEventListener('click', () => {
   let temp = currency1.value
   currency1.value = currency2.value
